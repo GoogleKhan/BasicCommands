fa3ae90874bc9e93f2deef7c609170ba540d65a8
GoogleKhan <98005652+GoogleKhan@users.noreply.github.com>
2022-07-11 11:30:44 +0530

    Update A.java

--- A.java
+++ A.java
@@ -3,4 +3,7 @@ public class A
 	public static void main(String[] abc){
 		System.out.println("Hello From main branch");
 	}
+	public static void hello(){
+		System.out.println("Hello From main branch");
+	}
 }
