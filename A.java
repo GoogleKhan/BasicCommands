5a168b20ca830a804113efa86633b472ecc9d036
GoogleKhan <shadab105662@gmail.com>
2022-07-11 13:28:14 +0530

    Pics and cmds

--- A.java
+++ A.java
@@ -2,6 +2,9 @@ public class A
 {
 	public static void main(String[] abc){
 		System.out.println("Hello From main branch");
+		System.out.println("Bye From main branch");
+		System.out.println("Hello From feature1 branch");
+		
 	}
 	public static void hello(){
 		System.out.println("Hello From main branch");
